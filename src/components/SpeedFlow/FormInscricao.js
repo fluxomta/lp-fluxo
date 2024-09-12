cfd16657acae0b84286d1b57dfeaa298c054fbe0
Tales Hs <webtailss@gmail.com>
2024-09-12 17:23:26 -0300

    Update speedflow'

--- src/components/SpeedFlow/FormInscricao.js
+++ src/components/SpeedFlow/FormInscricao.js
@@ -1,16 +1,23 @@
 // src/components/Indicadores/FormInscricao.js
+import Link from "next/link";
+import Icons from "@/components/Global/Icons";
 
 export default function FormInscricao() {
     return (
         <section
-            className=" hidden relative text-primary-500 bg-white">
+            className="relative text-white bg-primary-500">
             <div className="max-w-7xl px-4 py-12 mx-auto md:text-center">
-                <p className="mb-6 text-2xl font-base text-center md:text-3xl ">Caso você ainda não tenha assistido o <strong>Curso Gratuito de Fluxo</strong>, que eu elaborei para mostra o método <strong>SpeedFlow - essa ferramenta poderosa, aqui está sua oportunidade</strong>, assista e vire sua chave no <strong>Mercado Financeiro</strong>.</p>
-                <p className="p-4 text-2xl font-bold text-center border rounded-md md:text-4xl yellow black-gradient border-secondary-700">
-                    Cadastre seu email para desbloquear o método exclusivo SpeedFlow
-                </p>
+                <p className="mb-6 text-2xl font-bold text-center md:text-3xl ">Assine agora mesmo o SpeedFlow e <span className="yellow block">receba gratuito o meu Curso de Fluxo!</span></p>
+
                 <div className='w-full p-8 border rounded-md border-secondary-700 bg-primary-700 text-white'>
-                    &quot;vai entrar o form do active aqui&quot;
+
+                </div>
+                <div className="relative z-10 flex items-center justify-center mx-auto mt-8">
+                    <Link href="/lps/">
+                        <button className="flex items-center justify-center w-full group btn md:btn-large">
+                            Quero Agora Mesmo <Icons.AngleRight className='w-6 ml-2 transition-transform duration-300 transform group-hover:translate-x-1' />
+                        </button>
+                    </Link>
                 </div>
             </div>
         </section>
