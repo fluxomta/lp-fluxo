cdadd0c156cb09fa1cc890361f720c006d829e8e
Tales Hs <webtailss@gmail.com>
2024-09-17 17:40:43 -0300

    update whatasapp

--- src/components/Global/Footer.js
+++ src/components/Global/Footer.js
@@ -9,7 +9,7 @@ export default function Footer() {
                 <div className='max-w-6xl mx-auto flex flex-col items-center'>
                     <h3 className='font-bold text-2xl text-center w-full'>Ficou com qualquer dúvida, entre em contato!</h3>
                     <div className='rounded-full bg-[#0B6056] text-white p-2 mt-8 scale-95 hover:scale-105 transition-all duration-300'>
-                        <Link className="flex gap-2 items-center font-semibold" href="https://wa.me/5555418433088?text=Ol%C3%A1%2C+Estou+com+duvidas+sobre+o+Fluxo+MTA">
+                        <Link className="flex gap-2 items-center font-semibold" href="https://wa.me/55554196578795?text=Ol%C3%A1%2C+Estou+com+duvidas+sobre+o+Fluxo+MTA">
                             <Icons.Whatsapp className="w-8 text-white" /> Precisa de Ajuda?
                         </Link>
                     </div>
@@ -19,7 +19,7 @@ export default function Footer() {
 
                 {/* Precisa de Ajuda? */}
                 <div className='rounded-full bg-[#0B6056] text-white p-2 fixed right-0 m-8 bottom-10 z-30 scale-95 hover:scale-105 transition-all duration-300'>
-                    <Link className="flex gap-2 items-center font-semibold" href="https://wa.me/5555418433088?text=Ol%C3%A1%2C+Estou+com+duvidas+sobre+o+Fluxo+MTA">
+                    <Link className="flex gap-2 items-center font-semibold" href="https://wa.me/55554196578795?text=Ol%C3%A1%2C+Estou+com+duvidas+sobre+o+Fluxo+MTA">
                         <Icons.Whatsapp className="w-8 text-white" /> Precisa de Ajuda?
                     </Link>
                 </div>
