6946dd0f233651abc6853f9f94ad50ec54ab7ce8
Tales Hs <webtailss@gmail.com>
2024-09-17 12:11:24 -0300

    Update images for course

--- src/app/lps/speedflow/page.js
+++ src/app/lps/speedflow/page.js
@@ -7,10 +7,10 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 
 const aulas = [
-    { id: 1, name: "Cliente 1", message: "Depoimento incrível do cliente 1!", src: '/images/aulas/aula-01.png' },
-    { id: 2, name: "Cliente 2", message: "Depoimento incrível do cliente 2!", src: '/images/aulas/aula-02.png' },
-    { id: 3, name: "Cliente 3", message: "Depoimento incrível do cliente 3!", src: '/images/aulas/aula-03.png' },
-    { id: 4, name: "Cliente 4", message: "Depoimento incrível do cliente 4!", src: '/images/aulas/aula-04.png' },
+    { id: 1, name: "Aula 1", message: "Miniatura da Aula 1!", src: '/images/aulas/thumb-curso-01.png' },
+    { id: 2, name: "Aula 2", message: "Miniatura da Aula 2!", src: '/images/aulas/thumb-curso-02.png' },
+    { id: 3, name: "Aula 3", message: "Miniatura da Aula 3!", src: '/images/aulas/thumb-curso-03.png' },
+    { id: 4, name: "Aula 4", message: "Miniatura da Aaula 4!", src: '/images/aulas/thumb-curso-04.png' },
 ];
 
 export default function Home() {
@@ -22,9 +22,9 @@ export default function Home() {
                 <div className='absolute top-0 left-0 z-0 w-full h-full bg-opacity-20 bg-primary-500'></div>
                 <div className="max-w-6xl px-4 py-12 mx-auto md:text-lg relative z-10">
 
-                    <h1 className="mb-4 max-w-5xl mx-auto text-3xl font-bold text-center md:text-4xl gradient-yellow-text">
+                    <h1 className="mb-4 max-w-5xl mx-auto text-3xl font-bold text-center md:text-4xl text-white">
                         Chega de ter dúvidas no mercado financeiro, eu vou
-                        te ensinar de graça como ganhar dinheiro de verdade com o SpeedFlow
+                        te <span className='yellow'>ensinar de graça como ganhar dinheiro</span> de verdade com o SpeedFlow
                     </h1>
                     <div className="max-w-4xl pb-8 mx-auto ">
                         <Swiper
--- src/components/SpeedFlow/FormInscricao.js
+++ src/components/SpeedFlow/FormInscricao.js
@@ -8,10 +8,10 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 
 const aulas = [
-    { id: 1, name: "Cliente 1", message: "Depoimento incrível do cliente 1!", src: '/images/aulas/aula-01.png' },
-    { id: 2, name: "Cliente 2", message: "Depoimento incrível do cliente 2!", src: '/images/aulas/aula-02.png' },
-    { id: 3, name: "Cliente 3", message: "Depoimento incrível do cliente 3!", src: '/images/aulas/aula-03.png' },
-    { id: 4, name: "Cliente 4", message: "Depoimento incrível do cliente 4!", src: '/images/aulas/aula-04.png' },
+    { id: 1, name: "Aula 1", message: "Miniatura da Aula 1!", src: '/images/aulas/thumb-curso-01.png' },
+    { id: 2, name: "Aula 2", message: "Miniatura da Aula 2!", src: '/images/aulas/thumb-curso-02.png' },
+    { id: 3, name: "Aula 3", message: "Miniatura da Aula 3!", src: '/images/aulas/thumb-curso-03.png' },
+    { id: 4, name: "Aula 4", message: "Miniatura da Aaula 4!", src: '/images/aulas/thumb-curso-04.png' },
 ];
 
 export default function FormInscricao() {
