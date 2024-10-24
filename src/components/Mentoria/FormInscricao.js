c38a415f0321ed47f5aae0fa41a8aa86571e3784
Tales Hs <webtailss@gmail.com>
2024-10-23 23:40:20 -0300

    update form mentoria

--- src/components/Mentoria/FormInscricao.js
+++ src/components/Mentoria/FormInscricao.js
@@ -4,13 +4,13 @@
 import Link from "next/link";
 import Icons from "@/components/Global/Icons";
 import Image from 'next/image';
-import FormHubspot from "../Forms/FormHubspot";
+import FormMentoria from "../Forms/FormMentoria";
 
 export default function FormInscricao() {
     return (
         <section
             className="relative text-white bg-primary-500 group">
-            <FormHubspot label="Entrar na Lista de espera" origem="mentoria" />
+            <FormMentoria label="Entrar na Lista de espera" origem="mentoria" />
         </section >
     );
 }
