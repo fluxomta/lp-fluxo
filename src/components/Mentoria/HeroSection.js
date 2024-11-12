5ed412e25831d9579bf88f9a0190ed53781d76dc
Tales Hs <webtailss@gmail.com>
2024-11-11 22:43:08 -0300

    Update mentoria

--- src/components/Mentoria/HeroSection.js
+++ src/components/Mentoria/HeroSection.js
@@ -16,8 +16,8 @@ export default function HeroSection() {
             <div className='absolute top-0 left-0 z-0 w-full h-full bg-opacity-80 bg-primary-700'></div>
             <div className="flex flex-col justify-between items-center h-full max-w-7xl px-4 py-12 mx-auto relative z-10">
                 <div className="max-w-6xl mx-auto text-center">
-                    <p className="mb-4 text-4xl font-bold md:text-7xl">O Método definitivo para lucrar no <span className='yellow'>Mercado Financeiro</span></p>
-                    <h1 className='mb-4 text-4xl'>Mentoria <span className='yellow font-bold uppercase'>Fluxo simplificado 3.0</span></h1>
+                    <p className="mb-4 text-3xl font-bold md:text-7xl">O Método definitivo para lucrar no <span className='yellow'>Mercado Financeiro</span></p>
+                    <h1 className='mb-4 text-2xl'>Mentoria <span className='yellow font-bold uppercase'>Fluxo simplificado 3.0</span></h1>
 
                 </div>
                 <div className="w-full max-w-4xl p-1 mx-auto mb-8 mt-8">
